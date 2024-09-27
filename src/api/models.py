957e48ccc6287d1f2c1fef1ae6b7ac85e09985d4
Danny Valdivia <dluis.valdivia@gmail.com>
2024-09-27 17:04:38 +0000

    pop

--- src/api/models.py
+++ src/api/models.py
@@ -7,16 +7,37 @@ db = SQLAlchemy()
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean(), unique=False, nullable=False)
+    firs_name = db.Column(db.String(), unique=False, nullable=True)
+    last_name = db.Column(db.String(), unique=False, nullable=True)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User: {self.email}>'
 
     def serialize(self):
-         # do not serialize the password, its a security breach
-        return {"id": self.id,
-                "email": self.email,
-                "is_active": self.is_active}
-        
-#Model Table ()
+        # Do not serialize the password, its a security breach
+        return {'id': self.id,
+                'email': self.email,
+                'is_active': self.is_active}
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=False, nullable=False)
+    description = db.Column(db.String, unique=False, nullable=True)
+    body = db.Column(db.String, unique=False, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)  # Valor por defecto
+    image_url = db.Column(db.String)   # La url, la obtenmos de cloudinary
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, unique=False, nullable=False)
+
+
+class Medias(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    media_type = db.Column(db.Enum('image', 'video', 'podcast', name='media_type'))
+    url = db.Column(db.String)
